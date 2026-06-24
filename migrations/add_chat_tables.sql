a0da071bcc122bd8e4e79aa508d0fb9c78479e65
채란 <nemo20435@gmail.com>
2026-06-24 18:15:06 +0900

    [fix] sql 수정

--- migrations/add_chat_tables.sql
+++ migrations/add_chat_tables.sql
@@ -1,6 +1,6 @@
 -- 챗봇 세션 및 메시지 테이블
 -- 실행: mysql --default-character-set=utf8mb4 -u root -p next < migrations/add_chat_tables.sql
-
+USE `next`;
 CREATE TABLE IF NOT EXISTS chat_sessions (
     session_id  INT          NOT NULL AUTO_INCREMENT,
     owner_key   VARCHAR(255) NOT NULL COMMENT 'github_username',
